094dd5e04bc7f590e818f6b953809df4b3b44638
Luiz Otavio <luizomf@gmail.com>
2021-03-18 11:52:24 -0300

    Introdução ao NextAuth.js

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.Credentials({
+      name: 'Credentials',
+      credentials: {},
+      async authorize() {
+        // Add logic here to look up the user from the credentials supplied
+        const user = null;
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null;
+          // You can also Reject this callback with an Error or with a URL:
+          // throw new Error('error message') // Redirect to error page
+          // throw '/path/to/redirect'        // Redirect to a URL
+        }
+      },
+    }),
+  ],
+});
