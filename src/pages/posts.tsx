f43fb3f7499dbac0f9e2bcf32abf389f915d6d19
Luiz Otavio <luizomf@gmail.com>
2021-03-19 14:45:14 -0300

    Delete de posts

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps } from 'next';
 import { getSession, useSession } from 'next-auth/client';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import { Wrapper } from '../components/Wrapper';
 import { gqlClient } from '../graphql/client';
+import { GQL_MUTATION_DELETE_POST } from '../graphql/mutations/post';
 import { GQL_QUERY_GET_POSTS } from '../graphql/queries/post';
 import { frontEndRedirect } from '../utils/front-end-redirect';
 import { serverSideRedirect } from '../utils/server-side-redirect';
@@ -20,6 +22,12 @@ export type PostsPageProps = {
 
 export default function PostsPage({ posts = [] }: PostsPageProps) {
   const [session, loading] = useSession();
+  const [statePosts, setStatePosts] = useState(posts);
+  const [deleting, setDeleting] = useState(false);
+
+  useEffect(() => {
+    setStatePosts(posts);
+  }, [posts]);
 
   if (!session && !loading) {
     return frontEndRedirect();
@@ -31,15 +39,41 @@ export default function PostsPage({ posts = [] }: PostsPageProps) {
     return <p>Você não está autenticado</p>;
   }
 
+  const handleDelete = async (id: string) => {
+    setDeleting(true);
+
+    try {
+      await gqlClient.request(
+        GQL_MUTATION_DELETE_POST,
+        {
+          id,
+        },
+        {
+          Authorization: `Bearer ${session.accessToken}`,
+        },
+      );
+
+      setStatePosts((s) => s.filter((p) => p.id !== id));
+    } catch (e) {
+      alert('Não foi possível excluir este post');
+    }
+
+    setDeleting(false);
+  };
+
   return (
     <Wrapper>
       <h1>Olá {session?.user?.name || 'ninguém'}</h1>
 
-      {posts.map((p) => (
+      {statePosts.map((p) => (
         <p key={'post-' + p.id}>
           <Link href={`/${p.id}`}>
             <a>{p.title}</a>
-          </Link>
+          </Link>{' '}
+          |{' '}
+          <button onClick={() => handleDelete(p.id)} disabled={deleting}>
+            Excluir
+          </button>
         </p>
       ))}
     </Wrapper>
