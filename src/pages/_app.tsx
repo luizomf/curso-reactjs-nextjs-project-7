d190fedc2f72b28fd106f6ec3e5e34c35fc02598
Luiz Otavio <luizomf@gmail.com>
2021-03-19 17:19:47 -0300

    Loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { AppProps } from 'next/app';
 import { Provider as NextAuthProvider } from 'next-auth/client';
+import NextNprogress from 'nextjs-progressbar';
 
 import { GlobalStyles } from '../styles/global-styles';
 
@@ -9,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <ThemeProvider theme={theme}>
+        <NextNprogress
+          color={theme.colors.info}
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={10}
+        />
         <Component {...pageProps} />
         <GlobalStyles />
       </ThemeProvider>
